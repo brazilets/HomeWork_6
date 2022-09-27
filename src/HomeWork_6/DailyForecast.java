071391d6b4f0793628533fd993c51a3d702816e1
brazilets <brazilets161@gmail.com>
2022-09-27 21:56:33 +0300

    update

--- src/HomeWork_6/DailyForecast.java
+++ src/HomeWork_6/DailyForecast.java
@@ -3,6 +3,23 @@ package HomeWork_6;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "Date",
+        "EpochDate",
+        "Temperature",
+        "Day",
+        "Night",
+        "Sources",
+        "MobileLink",
+        "Link"
+})
 public class DailyForecast {
 
     @JsonProperty("Date")
